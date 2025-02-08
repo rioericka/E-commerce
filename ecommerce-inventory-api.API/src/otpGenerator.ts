96197d011dd8ea6ea2015aca296284679fc51e4a
rioericka <rioericka9@gmail.com>
2025-02-08 21:15:20 +0800

    Delete ecommerce-inventory-api.API directory

--- ecommerce-inventory-api.API/src/otpGenerator.ts
+++
@@ -1,7 +0,0 @@
-import crypto from 'crypto';
-
-// Function to generate a random OTP (6 digits)
-export function generateOTP(): string {
-  const otp = crypto.randomInt(100000, 999999); // Generate 6-digit OTP
-  return otp.toString();
-}
