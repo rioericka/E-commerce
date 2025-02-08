4fc265a928d06da04727e2edda859504bc1322a5
rioericka <rioericka9@gmail.com>
2025-02-08 19:14:11 +0800

    Delete ecommerce-inventory-api.API directory

--- ecommerce-inventory-api.API/src/interface/orderInterface.ts
+++
@@ -1,9 +0,0 @@
-import { Document } from "mongoose";
-
-export interface IOrder extends Document {
-
-  orderId: number;       // ID linking the detail to an order
-  productId: number;     // ID of the product in the order
-  quantity: number;      // Quantity of the product in the order
-  price: number;         // Price of the product
-}
