5e3be6fa75a73f97308e233dedf97daf2968da8e
rioericka <rioericka9@gmail.com>
2025-02-08 20:37:43 +0800

    Delete ecommerce-inventory-api.API directory

--- ecommerce-inventory-api.API/src/interface/supplierInterface.ts
+++
@@ -1,8 +0,0 @@
-import { Document } from "mongoose";
-
-export interface ISupplier extends Document {
-  supplierId: string;      // Unique identifier for the supplier
-  supplierName: string;    // Name of the supplier
-  contactInfo: string;     // Contact information (e.g., phone, email)
-  address: string;         // Physical address of the supplier
-}
--- ecommerce-inventory-api.API/src/interface/userInterface.ts
+++
@@ -1,8 +0,0 @@
-import { Document } from "mongoose";
-
-export interface IUser extends Document {
-  email: string;
-  password: string;
-  firstName: string;
-  lastName: string;
-}
